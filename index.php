41835cf7512468add89c639a5df0d987585bcd55
itsWaterBoi <126034069+itsWaterBoi@users.noreply.github.com>
2023-02-25 05:31:04 -0500

    Get some basic info about me inside
    Get basic info inside aka just put my name and pfp and stuff

--- index.php
+++ index.php
@@ -2,4 +2,19 @@
 <?php include_once("core/inc/header.php"); ?>
 
 <br><br><br>
-<h1>Hi</h1>
+<h1>👋 Hi, I'm WaterBoi!</h1>
+<h2>I develop things and like to mess around with roblox clients</h2>
+<h3>You can see some of my posts and projects below</h3>
+
+<div id="posts">
+    <h1>Posts</h1>
+    <p>Coming Soon!</p>
+</div>
+<div id="projects">
+    <h1>Projects</h1>
+    <p>Coming Soon!</p>
+    <small><a href="/projects.php">view all</a></small>
+</div>
+
+</body>
+</html>
